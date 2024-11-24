6007abbc6b8ef068c5875ebc326998d3bcd2a1c5
Michał Klepner <michal.klepner1@o2.pl>
2024-11-24 12:15:19 +0100

    Added some styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,54 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 import styled from "styled-components";
+import pulsingCoin from "./../public/images/pulsing_coin.gif";
 
-const Test = styled.h1`
-  font-size: 50px;
-  color: var(--color-yellow-100);
+const Header = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 30px;
+  color: #89cff0;
+  text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.5);
 `;
 
-const Contener = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  padding: 20px;
   text-align: center;
-  align-items: center;
-  width: auto;
 `;
 
+const CurrencyContainer = styled.div`
+  border: 2px solid purple;
+`;
 
+const ConverterBox = styled.div`
+  background: rgba(44, 47, 51, 0.8);
+  border-radius: 15px;
+  padding: 30px;
+  box-shadow: 0 8px 20px rgba(0, 0, 0, 0.5);
+  width: 100%;
+  max-width: 500px;
+`;
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Field = styled.div`
+  margin-bottom: 20px;
+`;
+
+const GifImage = styled.img``;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <Contener>
-      <Test>Welcome to Currency Converter</Test>
-      <a href="https://vite.dev" target="_blank">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://react.dev" target="_blank">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-      </a>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </Contener>
+    <Container>
+      <Header>Welcome to Currency Converter</Header>
+
+      <ConverterBox>
+        <Field></Field>
+      </ConverterBox>
+    </Container>
   );
 }
 
